fea216871a4d06d04f0a525d91de7ace57f44bf4
Matt Sadowski <matt.sadowski@gmail.com>
2020-06-04 00:24:47 -0400

    Finished UFO-level-1 code, started UFO-level-2 code.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,42 @@
-// from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+button.on("click", filterTable);
+form.on("submit", filterTable);
+
+function filterTable() {
+    d3.event.preventDefault();
+        
+    var inputElement, inputValue, table, tr, i;
+    inputElement = d3.select("#datetime");
+    inputValue = inputElement.property("value");
+    table = document.getElementById("ufo-table");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerText === inputValue) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+    var filteredSightings = tableData.filter(sighting => sighting.datetime === inputValue);
+    console.log(filteredSightings);
+};
+
+
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,59 @@
-// from data.js
-var tableData = data;
+var tableData, tbody;
+tableData = data;
+tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button, form;
+button = d3.select("#filter-btn");
+form = d3.select("#form");
+
+button.on("click", filterTable);
+form.on("submit", filterTable);
+
+
+// var form = document.getElementById("form");
+// var inputId = form.getElementsByTagName("input");
+// for (i = 0; i < inputId.length; i++) {
+//     var inputArray = []
+//     inputArray.push(this.id);
+// };
+
+
+function filterTable() {
+    d3.event.preventDefault();
+
+    var form, inputId;
+    form = document.getElementById("form");
+    inputId = form.getElementsByTagName("input");
+
+    for (var i = 0; i < inputId.length; i++) {
+
+        var inputElement, inputValue, table, tr;
+        inputElement = d3.select(this.id);
+        inputValue = inputElement.property("value");
+        table = document.getElementById("ufo-table");
+        tr = table.getElementsByTagName("tr");
+
+        for (var j = 0; j < tr.length; j++) {
+            td = tr[j].getElementsByTagName("td")[0];
+            if (td) {
+                if (td.innerText === inputValue) {
+                    tr[JSON].style.display = "";
+                } else {
+                    tr[j].style.display = "none";
+                }
+            }
+        }
+
+        var filteredSightings = tableData.filter(sighting => sighting.datetime === inputValue);
+        console.log(filteredSightings);
+    }
+};
+
